f0090b2d146d0234deeb44c99abf8f613e3f43aa
Stremya <saskefight@gmail.com>
2026-06-20 23:37:26 +0900

    все готово для работы

---
+++ Roguelike/ViewsModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿/*
+связующее звено между View и Model:
+Свойства для отображения: PlayerHP, CurrentLevel, Score
+Команды для управления (MovementCommand)
+Ссылка на GameEngine
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike.ViewsModels
+{
+    internal class MainViewModel
+    {
+    }
+}
